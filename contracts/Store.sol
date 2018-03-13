39e76c9f50ae4dad9b4d8963a1113244640c8ea2
bobomurod <bobomurod.boboev@yandex.ru>
2018-03-13 16:13:54 +0300

    ended Store Contarct

--- contracts/Store.sol
+++ contracts/Store.sol
@@ -1,5 +1,17 @@
 pragma solidity ^0.4.18;
 
 contract SimpleStorage{
-    
+    uint x;
+    function get() public constant returns(uint){
+        return x;
+    }
+
+
+    funciton set(uint x_) public {
+        x = x_;
+    }
+
+    function seyHi() public constant returns(uint){
+        return true;
+    }
 }
